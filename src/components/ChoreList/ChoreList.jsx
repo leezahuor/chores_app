259ed6dd14b79590dd2354d69206e88cd0afec77
leezahuor <leezahuor@gmail.com>
2025-07-29 14:45:03 -0700

    added tests for ChoreList

--- src/components/ChoreList/ChoreList.jsx
+++ src/components/ChoreList/ChoreList.jsx
@@ -1,7 +1,9 @@
+import React from "react";
 import "./ChoreList.css";
 import * as C from "./ChoreListConstants";
 
 function ChoreList({ chores }) {
+
   return (
     <div className="chore-list-container">
       <h2 className="chore-list-title">Chores</h2>
@@ -11,7 +13,7 @@ function ChoreList({ chores }) {
           <p className="chore-empty-message">{C.CHORE_LIST_EMPTY_MSG}</p>
         ) : (
           chores.map((chore, index) => (
-            <div className="chore-card" key={index}>
+            <div className="chore-card" key={index} data-testid="chore-list">
               <div className="chore-name">{chore.choreName}</div>
               <div className="chore-info">
                 <span>Due: {new Date(chore.dueDate).toLocaleDateString()}</span>
